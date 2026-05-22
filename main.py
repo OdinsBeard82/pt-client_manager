6cb06724703442e9b6657cd089715cd6267a4944
Daniel Charles <danielcharles_500@hotmail.com>
2026-05-22 08:25:42 +0100

    Fix CORS: add middleware to app and allow port 5173

--- main.py
+++ main.py
@@ -11,11 +11,21 @@ Base.metadata.create_all(bind=engine)
 origins = [
     "http://localhost",
     "http://localhost:3000",
+    "http://localhost:5173",
     "http://localhost:8000",
     "http://localhost:8080",
     ]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def read_root():    return {"Hello": "World"}
 
