add2e9562e249bd0a3d5786a8ee8302588315359
Daniel Charles <danielcharles_500@hotmail.com>
2026-05-22 08:22:14 +0100

    Scaffold Vite React app and wire App.jsx to ClientList

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import ClientList from "./components/clientList";
+
+function App() {
+  return (
+    <>
+      <h1>PT Client Manager</h1>
+      <ClientList />
+    </>
+  );
+}
+
+export default App;
