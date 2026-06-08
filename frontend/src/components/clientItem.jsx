de8c0e3c4498637544a49738e898845044574dd6
Daniel Charles <danielcharles_500@hotmail.com>
2026-06-08 08:56:02 +0100

    Wire edit feature into ClientItem with isEditing toggle

--- frontend/src/components/clientItem.jsx
+++ frontend/src/components/clientItem.jsx
@@ -1,4 +1,13 @@
-function ClientItem({ client, onDelete }) {
+import { useEffect, useState } from "react";
+import EditClientForm from "./EditClientForm";
+
+function ClientItem({ client, onDelete, onClientUpdated }) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function editClientForm() {
+        setIsEditing(true)
+    }
+
     function confirm1() {
         if (window.confirm(`Delete ${client.name}?`)) {
             onDelete(client.id);
@@ -6,7 +15,15 @@ function ClientItem({ client, onDelete }) {
     }
     
 return (
+    isEditing ? <EditClientForm 
+        client={client} 
+        onClientUpdated={(updatedClient) => {
+            onClientUpdated(updatedClient);
+            setIsEditing(false);
+        }} 
+    /> : 
     <div>
+        <button onClick={editClientForm}>Edit button</button>
         <button onClick={confirm1}>Delete client</button>
         <h3>{client.name}</h3>
         <p>Email: {client.email}</p>
